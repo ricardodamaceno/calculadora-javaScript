4aeb15863fefd388685297d879b497600959e8a2
Ricardo da Maceno <99434415+ricardodamaceno@users.noreply.github.com>
2022-08-19 15:06:38 -0300

    craindo calculador javaScript

---
+++ calculadora-javaScript/script.js
@@ -0,0 +1,25 @@
+// Declarativa
+// function funcao(){
+//     console.log('Função declarativa');
+// } 
+// funcao();
+
+//Declaração da funcão com nomeação
+
+// var funcao = function declaracaoFuncao(){
+//     console.log('Declaração da funcão com nomeação');
+// }
+// funcao();
+
+//Declaração da funcão sem nomeação
+
+// var funcao = function (){
+//     console.log('Declaração da funcão sem nomeação');
+// }
+// funcao(); 
+
+//arrow function
+// var funcao = () =>{
+//     console.log('Arrow Function');
+// }
+// funcao();
